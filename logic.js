3c3fdc7584b42dd234167a0972dfd0ff25f493d6
Nikhil Saini <nikhil.nextbyts@gmail.com>
2024-10-24 22:05:46 +0530

    do some changes and add some files

--- logic.js
+++ logic.js
@@ -1,18 +1,20 @@
 // change the color mode
 
-let modeBtn = document.querySelector("#mode");
+let modeBtn = document.querySelector("button");
 let mode = "light";
-let body = document.querySelector("body");
+let body = document.querySelector(".main");
 
 modeBtn.addEventListener("click", () => {
     if(mode === "light") {
         mode = "dark";
         body.classList.add("dark");
         body.classList.remove("light");
+        console.log("hello");
     }
     else {
         mode = "light";
         body.classList.add("light");
         body.classList.remove("dark");
+        console.log("hello");
     }
 });
