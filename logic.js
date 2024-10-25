c89ef679c0995b6265e5fb412ee458b1f1d862fd
Nikhil Saini <nikhil.nextbyts@gmail.com>
2024-10-25 23:36:26 +0530

    do some changes

--- logic.js
+++ logic.js
@@ -1,20 +1,35 @@
 // change the color mode
 
-let modeBtn = document.querySelector("button");
+let modeBtn = document.querySelector(".mode");
 let mode = "light";
-let body = document.querySelector(".main");
+let body = document.querySelector(".mainContainer");
 
 modeBtn.addEventListener("click", () => {
     if(mode === "light") {
         mode = "dark";
         body.classList.add("dark");
         body.classList.remove("light");
-        console.log("hello");
+        console.log("enable dark mode");
+        modeBtn.textContent = "Light Mode";
+        modeBtn.classList.add("dark");
+        modeBtn.classList.remove("light");
     }
     else {
         mode = "light";
         body.classList.add("light");
         body.classList.remove("dark");
-        console.log("hello");
+        console.log("enable light mode");
+        modeBtn.textContent = "Dark Mode";
+        modeBtn.classList.add("light");
+        modeBtn.classList.remove("dark");
     }
+});
+
+// logic for menu button
+
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
 });
